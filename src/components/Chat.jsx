715ccaed56f8ac97f78657246283526bfd004f30
Omer Meidan <Ome12001@gmail.com>
2023-02-27 17:04:12 +0200

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,8 @@
 import React , {useRef,useState} from 'react';
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { ChatContainer, MessageList, Message, MessageInput,ConversationHeader,Avatar,VoiceCallButton,VideoCallButton,InfoButton } from '@chatscope/chat-ui-kit-react';
 import NavBar from './NavBar';
-function Chat(props) {
+function Chat() {
     const inputRef = useRef();
     const [messages, setMessages] = useState([]);
   
@@ -12,30 +13,42 @@ function Chat(props) {
       }]);
       inputRef.current.focus();
     };
+
+
+    
     return (
         <div>
+            {console.log(styles)}
              <NavBar/>
-        <div style={{
-            height: "500px"
-          }}>                
-                                <ChatContainer style={{width:'100vw',height:'92vh'}}>
-                                    <ConversationHeader>
-                                        <Avatar src='https://chatscope.io/storybook/react/static/media/zoe.e31a4ff8.svg' name="Kai" />
-                                        <ConversationHeader.Content userName="המורה שלי" info="מחובר/ת" />
-                                        <ConversationHeader.Actions>
-                                            <VoiceCallButton />
-                                            <VideoCallButton />
-                                            <InfoButton />
-                                        </ConversationHeader.Actions>          
-                                        </ConversationHeader>
-                                        <MessageList scrollBehavior="smooth">
-                                          {messages.map((m, i) => <Message key={i} model={m} />)}
-                                        </MessageList>
-                                        <MessageInput placeholder="Type message here" onSend={handleSend} ref={inputRef} />
-                              </ChatContainer>
-                        </div>
+             <div style={{
+    height: "500px"
+  }}>                
+                        <ChatContainer style={{width:'100vw',height:'92vh'}}>
+                            <ConversationHeader>
+                                <Avatar src='https://chatscope.io/storybook/react/static/media/zoe.e31a4ff8.svg' name="Kai" />
+                                <ConversationHeader.Content userName="המורה שלי" info="מחובר/ת" />
+                                <ConversationHeader.Actions>
+                                    <VoiceCallButton />
+                                    <VideoCallButton />
+                                    <InfoButton />
+                                </ConversationHeader.Actions>          
+                                </ConversationHeader>
+                                <MessageList scrollBehavior="smooth">
+                                  {messages.map((m, i) => <Message key={i} model={m} />)}
+                                </MessageList>
+                                <MessageInput placeholder="Type message here" onSend={handleSend} ref={inputRef} />
+                      </ChatContainer>
+                </div>;
                         </div>
     );
 }
 
 export default Chat;
+
+
+
+
+
+
+
+
